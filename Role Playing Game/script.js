fefb6325ce232b40a5ce272284c4a26f2df49494
bienvenue cedrick <90506492+cedrick13bienvenue@users.noreply.github.com>
2024-10-01 22:14:17 +0200

    Updated script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -168,9 +168,20 @@ function attack() {
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
     lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
   }
 }
 
 function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+
+}
+
+function lose() {
 
 }
