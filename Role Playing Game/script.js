a70cac508474b4282bbb5fe8768c011e1129c41e
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-02-01 22:37:27 +0200

    location store added

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -50,13 +50,14 @@ const locations = [
     "button functions": [goStore, goCave, fightDragon],
     text: "You are in the town square. You see a sign that says \"Store\"."
   },
-  
+
   {
     name: "store",
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
   },
+  
   {
     name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
