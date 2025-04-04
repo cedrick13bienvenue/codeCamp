0262478fe167dcf85dc5e4c8d2c3ae1c46d3d630
bienvenue cedrick <90506492+cedrick13bienvenue@users.noreply.github.com>
2025-04-04 20:12:09 +0200

    New script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -147,7 +147,9 @@ function buyWeapon() {
       text.innerText += " In your inventory you have: " + inventory;
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
+      
     }
+    
   } else {
     text.innerText = "You already have the most powerful weapon!";
     button2.innerText = "Sell weapon for 15 gold";
